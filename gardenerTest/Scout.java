77623d0752150ebbf7d9fe2aebaff0320d125557
tesu <jasonlam@mit.edu>
2017-01-24 20:39:16 -0500

    elementary movement for scouts

--- gardenerTest/Scout.java
+++ gardenerTest/Scout.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class Scout {
     static int status = 0;
-    static int timer = 0;
+    static MapLocation target = null;
 
     public static void run(RobotController rc) {
         Random rand = new Random(rc.getID());
@@ -25,14 +25,64 @@ public class Scout {
                 Utils.alwaysRun(rc);
 
                 System.out.println(status);
-                timer++;
 
-                Utils.moveTowards(rc, face);
+                int foundEnemies = rc.readBroadcast(0);
+                RobotInfo[] enemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+
+                switch (status) {
+                    case 0:
+                        boolean found = false;
+                        for (RobotInfo r : enemies) {
+                            if (r.getType() == RobotType.ARCHON || r.getType() == RobotType.GARDENER) {
+                                found = true;
+                                rc.broadcast(foundEnemies*2+1, Math.round(r.getLocation().x));
+                                rc.broadcast(foundEnemies*2+2, Math.round(r.getLocation().y));
+                                target = r.getLocation();
+                                face = rc.getLocation().directionTo(target);
+                                foundEnemies++;
+                            }
+                        }
+                        if (foundEnemies == 0) {
+                            while (!Utils.moveTowards(rc, face)) {
+                                face =  new Direction(2 * (float) Math.PI * rand.nextFloat());
+                            }
+                            break;
+                        } else {
+                            nextStage();
+                        }
+                    case 1:
+                        if (target == null) {
+                            int t = rc.getID() % foundEnemies;
+                            int x = rc.readBroadcast(t * 2 + 1);
+                            int y = rc.readBroadcast(t * 2 + 2);
+                            target = new MapLocation(x, y);
+                            face = rc.getLocation().directionTo(target);
+                        }
+                        if (!rc.canSenseLocation(target)) {
+                            while (!Utils.moveTowards(rc, face)) {
+                                face = new Direction(2 * (float) Math.PI * rand.nextFloat());
+                            }
+                            break;
+                        } else {
+                            nextStage();
+                        }
+                    case 2:
+                        if (enemies.length == 0) {
+                            target = null;
+                            status = 1;
+                            break;
+                        }
+                    default:
+                }
 
                 Clock.yield();
             } catch (GameActionException e) {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    public static void nextStage() {
+        status++;
     }
 }
