011be6aaf2c945458a0f9935c2cbcd4492e3bfb2
tesu <jasonlam@mit.edu>
2017-01-24 20:53:12 -0500

    some sort of dodging algo

--- skeleton/Utils.java
+++ skeleton/Utils.java
@@ -2,6 +2,8 @@ package skeleton;
 
 import battlecode.common.*;
 
+import java.awt.*;
+
 public class Utils {
     public static void alwaysRun(RobotController rc) throws GameActionException {
         for (TreeInfo tree : rc.senseNearbyTrees()) {
@@ -20,32 +22,47 @@ public class Utils {
         return false;
     }
 
-    public static MapLocation location(RobotController rc, int i) {
-        return rc.getLocation().add(new Direction((float)i/10*2*(float)Math.PI), rc.getType().strideRadius);
-    }
-
     public static boolean moveTowards(RobotController rc, Direction d) throws GameActionException {
         int[] directions = {0,0,0,0,0,0,0,0,0,0};
 
-        for (BulletInfo b : rc.senseNearbyBullets()) {
+        if (rc.hasMoved()) return false;
 
+        for (BulletInfo b : rc.senseNearbyBullets(5)) {
+            for (int i = 0; i < directions.length; i++) {
+                if (willCollide(rc, b, rc.getLocation().add(new Direction((float)i/directions.length*2*(float)Math.PI)))) {
+                    directions[i] -= 10;
+                }
+            }
         }
 
-        if (rc.canMove(d)) {
+        if (rc.canMove(d) && directions[(int)d.getAngleDegrees()/directions.length] >= 0) {
             rc.move(d);
             return true;
         }
         for (int i = 1; i < 10; i++) {
-            if (rc.canMove(d.rotateRightDegrees(i*10))) {
-                rc.move(d.rotateRightDegrees(i*10));
+            Direction t = d.rotateRightDegrees(i*10);
+            if (rc.canMove(t) && directions[(int)t.getAngleDegrees()/directions.length] >= 0) {
+                rc.move(t);
                 return true;
             }
-            if (rc.canMove(d.rotateLeftDegrees(i*10))) {
-                rc.move(d.rotateLeftDegrees(i*10));
+            t = d.rotateLeftDegrees(i*10);
+            if (rc.canMove(t) && directions[(int)t.getAngleDegrees()/directions.length] >= 0) {
+                rc.move(t);
                 return true;
             }
         }
         return false;
+    }
+
+    public static boolean willCollide(RobotController rc, BulletInfo b, MapLocation m) {
+        if (m.distanceTo(b.location) < rc.getType().bodyRadius) return true;
+
+        Direction directionToRobot = b.location.directionTo(m);
+        float theta = b.dir.radiansBetween(directionToRobot);
+
+        if (Math.abs(theta) > Math.PI/2) return false;
+
+        return ((float)Math.abs(b.location.distanceTo(m) * Math.sin(theta)) <= rc.getType().bodyRadius);
     }
 
     public static class RobotAnalysis {
